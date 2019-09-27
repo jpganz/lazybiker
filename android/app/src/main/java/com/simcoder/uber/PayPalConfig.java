652257bf6e8347db3110bccd20e02da6f8132d6b
Juan Hernandez <=>
2019-09-27 09:29:53 +0200

    Adding project

---
+++ android/app/src/main/java/com/simcoder/uber/PayPalConfig.java
@@ -0,0 +1,7 @@
+package com.simcoder.uber;
+
+public class PayPalConfig {
+    public static final String PAYPAL_CLIENT_ID = "AUt4IWneqUcA0x2UYacjXJc0o1gju6IoFmK078cAIsMc-T60KiqL6kB8jQAHBbEVdlHxt4F0_JU8mdxB";
+
+    public static final String PAYPAL_PAYOUT_URL = "https://us-central1-lazybiker-9b4ea.cloudfunctions.net/payout";
+}
